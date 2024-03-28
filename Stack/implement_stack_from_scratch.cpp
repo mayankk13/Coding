9c1d795ca9192754e8e3f65473c9bb21f1e8516d
Kumar Mayank <mayank13b@gmail.com>
2024-03-28 09:26:57 +0530

    minor change

--- Stack/implement_stack_from_scratch.cpp
+++ Stack/implement_stack_from_scratch.cpp
@@ -7,7 +7,32 @@ private:
     int top = -1;
 public:
     void push(int x) {
-        top++;
-        arr[top] = x;
+        arr[++top] = x;
+    }
+    int pop() {
+        if (top == -1) {
+            return -1;
+        }
+        return arr[top--];
+    }
+
+    bool isEmpty() {
+        return top < 0;
+    }
+
+    int peek() {
+        return arr[top];
     }
 };
+
+int main() {
+    Solution ob;
+    ob.push(10);
+    ob.push(2);
+    ob.push(23);
+    cout<<ob.pop() <<endl;
+    cout<<ob.isEmpty() <<endl;
+    cout<<ob.peek() <<endl;
+
+    return 0;
+}
