839c08fbab9b175544e86b67441535732605fe68
Kumar Mayank <mayank13b@gmail.com>
2024-04-02 01:48:45 +0530

    mid element of stack

--- Stack/implement_stack_from_scratch.cpp
+++ Stack/implement_stack_from_scratch.cpp
@@ -1,19 +1,52 @@
+// implement stack from scratch
+
 #include<bits/stdc++.h>
 using namespace std;
+#define max_size 100
 
 class Solution {
 private:
-    int arr[1000];
-    int top = -1;
+    int arr[max_size]; // array to store stack element
+    int top = -1;      // top of the stack
+
 public:
     void push(int x) {
+        if (isFull()) {
+            cout<<"Stack is Full" <<endl;
+        }
         arr[++top] = x;
     }
     int pop() {
-        if (top == -1) {
+        if (isEmpty()) {
+            cout<<"Stack is empty" <<endl;
             return -1;
         }
-        return arr[top--];
+        return arr[top--];  
+    }
+
+    int middleElement() {
+        if (isEmpty()) {
+            cout<<"Stack is empty" <<endl;
+        }
+
+        int size = top + 1;
+        int mid = size / 2;
+
+        int temp[max_size]; // array to store stack elements;
+        int middleElement = -1; // initialize to a default value
+
+        for (int i = 0; i < mid; i++) {
+            int element = pop();
+            temp[i] = element;
+        }
+
+        middleElement = pop();
+
+        for (int i = mid - 1; i >= 0; i--) {
+            push(temp[i]);
+        }
+
+        return middleElement;
     }
 
     bool isEmpty() {
@@ -23,6 +56,10 @@ public:
     int peek() {
         return arr[top];
     }
+
+    bool isFull() {
+        return top == max_size - 1;
+    }
 };
 
 int main() {
@@ -30,9 +67,12 @@ int main() {
     ob.push(10);
     ob.push(2);
     ob.push(23);
+    ob.push(0);
+    ob.push(99);
     cout<<ob.pop() <<endl;
     cout<<ob.isEmpty() <<endl;
     cout<<ob.peek() <<endl;
+    cout<<ob.middleElement() <<endl;
 
     return 0;
 }
