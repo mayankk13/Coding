80c8c50bf3c9558136e12dbb3a1222639a12266e
Kumar Mayank <mayank13b@gmail.com>
2024-04-28 01:12:59 +0530

    mc

--- Tree/BinaryTree/103.zigzag_Level_Order_Traversal.cpp
+++ Tree/BinaryTree/103.zigzag_Level_Order_Traversal.cpp
@@ -1,3 +1,17 @@
+/*
+Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, 
+then right to left for the next level and alternate between).
+
+        3
+      /   \
+     9     20
+          /   \
+        15     7 
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[20,9],[15,7]]
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
