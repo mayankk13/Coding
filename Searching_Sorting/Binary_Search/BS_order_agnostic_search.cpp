35380ed18f7d8ae3668da4490c7fc3fe4e433bff
Kumar Mayank <mayank13b@gmail.com>
2024-08-19 01:36:13 +0530

    order agnostic search

---
+++ Searching_Sorting/Binary_Search/BS_order_agnostic_search.cpp
@@ -0,0 +1,55 @@
+// Binary search
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+    public:
+    int binarySearch(vector<int>& arr, int target)  {
+        int low = 0;
+        int high = arr.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[low] > arr[high]) {
+                if (arr[mid] > target) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
+            }
+            else {
+                if (arr[mid] > target) {
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }    
+        }
+    }
+};
+
+int main() {
+    int m;
+    cin >> m;
+    
+    vector<int> arr1(m);
+
+    // Input elements for arr1
+    for (int i = 0; i < m; i++) {
+        cin >> arr1[i];
+    }
+
+    int x;
+    cin>> x;
+    Solution ob;
+    cout<<ob.binarySearch(arr1, x) <<endl;
+
+    return 0;
+}
