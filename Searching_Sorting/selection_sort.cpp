b5cb948c7bfc4881787fcfc5eee45fc1fc2ad0af
Kumar Mayank <mayank13b@gmail.com>
2025-01-20 00:39:35 +0530

    selection sort

--- Searching_Sorting/selection_sort.cpp
+++ Searching_Sorting/selection_sort.cpp
@@ -1,29 +1,35 @@
+/*
+Selection sort: 
+    1. First we find the smallest element and swap it with the first element. This way we get the smallest element 
+    at its correct position.
+    2. Then we find the smallest among remaining elements (or second smallest) and swap it with the second element.
+
+input: 64, 34, 25, 12, 22, 11, 90
+output: 11 12 22 25 34 64 90
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
 class Solution {
-public:
-    int selectionSort(int arr[], int n) {
-        int index;
+  public:
+    // Function to perform selection sort on the given array.
+    void selectionSort(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        // n-1 because we are doing j = i+1
         for (int i = 0; i < n-1; i++) {
-            index = i;
-            for (int j = i+1; j < n; j++) {
-                if (arr[j] < arr[index]) {
-                    index = j;
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
             }
-            if (index != i) {
-                int temp = arr[i];
-                arr[i] = arr[index];
-                arr[index] = temp;
+            
+            if (i != minIndex) {
+                swap(arr[i], arr[minIndex]);
             }
         }
-        for (int i = 0; i < n; i++) {
-            cout<<arr[i] <<" ";
-        }
-        cout<<endl;
-
-        return 0;
     }
 };
 
