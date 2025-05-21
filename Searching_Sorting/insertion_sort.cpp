2b007df5e7f899167c30bd15d7b1b40c8f90ff17
Kumar Mayank <mayank13b@gmail.com>
2025-05-22 02:02:08 +0530

    subsequenceSum equals k

--- Searching_Sorting/insertion_sort.cpp
+++ Searching_Sorting/insertion_sort.cpp
@@ -5,7 +5,7 @@ class Solution {
 public:
     void insertionSort(int arr[], int n) {
         for (int i = 1; i < n; i++) {
-            // i started with second element as first element is already sorted.
+            // i started with second element as we assume first element is already sorted.
             int key = arr[i];   // for every element we will store in a variable and find its correct position in left sorted array.
             int j = i - 1;
             // we will start from the first left to the key element and will go till the first element and
